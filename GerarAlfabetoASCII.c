0eebb8465bd66220423a5a130b3a14212682b495
lcpdavii <162602242+lcpdavii@users.noreply.github.com>
2024-03-13 11:10:31 -0300

    GerarAlfabetoASCII.c

--- GerarAlfabetoASCII.c
+++ GerarAlfabetoASCII.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-/*para que voce consiga elaboras o alfabeto, mas quando for colocar o scanf, no char voce coloca %c, dai voce pega os numeros da tabela ascii
+/*para que voce consiga elaboras o alfabeto, quando for colocar o scanf, no char voce coloca %c, dai voce pega os numeros da tabela ascii
 que correspondam as letras, e coloca os numeros correspondentes.*/
 int main(){
     for(int i = 97; i <= 122; i++){
